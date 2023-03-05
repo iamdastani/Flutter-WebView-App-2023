db81223c9927f0f34e6ef219a155f6ee55d0afd8
Dastani Ferdinandi <iamdastani@gmail.com>
2023-03-05 04:07:08 +0300

    ecommerce

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body : SafeArea (
       child: WebView(
-       initialUrl: 'https://beiizetu.shop',
+       initialUrl: 'https://onlinesoln1.com',
        javascriptMode: JavascriptMode.unrestricted,
       onWebViewCreated: (WebViewController controller) {
         _controller = controller;
