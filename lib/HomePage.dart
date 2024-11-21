c1e89aa71d5c633f2006d0ea30d3534145c21176
Dastani Ferdinandi <iamdastani@gmail.com>
2024-11-21 04:09:11 +0300

    Flutter webview updated

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,26 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  WebViewController? _controller;
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate; // Allow all URLs to load
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://maishaforum.com'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WebView(
-          initialUrl: 'https://www.maishaforum.com/',
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController controller) {
-            _controller = controller;
-          },
-        ),
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
